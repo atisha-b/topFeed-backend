39a77fb96a991721440dd47123a4edf85382bca2
atisha-b <ishabhattacharya45@gmail.com>
2024-09-15 01:20:38 +0530

    first commit

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import redditRoutes from './src/routes/redditRoutes.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
-app.use(express.json());
-
-app.use('/api/reddit', redditRoutes);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
